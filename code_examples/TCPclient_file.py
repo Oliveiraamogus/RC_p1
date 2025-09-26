57992d19a22c2c3cd15679d940dc0eb7edddabde
Oliveiraamogus <mpi.oliveira@campus.fct.unl.pt>
2025-09-26 11:01:06 +0100

    started doing main on client

--- code_examples/TCPclient_file.py
+++ code_examples/TCPclient_file.py
@@ -6,31 +6,56 @@
 from socket import *
 import sys
 
-args = (sys.argv[1]).split(':')
-
-serverName = args[0]            # server name
-serverPort = int(args[1])                  # socket server port number
-sockBuffer = 1024                   # socket buffer size
+serverName = sys.argv[1]            # server name
+serverPort = int(sys.argv[2])                  # socket server port number
+sockBuffer = 512                   # socket buffer size
 
 def main():
+  try:
+    clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
+    clientSocket.connect((serverName, serverPort))   # open TCP connection
+  except ConnectionRefusedError:
+    print("Connection Refused")
+    sys.exit()
 
-  nameInServer = args[2]
-  
+  print("Connect to server")
+  print(clientSocket.recv(sockBuffer).decode()) 
 
-  clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
-  clientSocket.connect((serverName, serverPort))   # open TCP connection
+  sentence = [""]
+  while (sentence[0] != "end") :
+    try:
+      sentence = (input ("Command: ")).split()
+      match sentence[0]:
+        case "dir":
+          clientSocket.send(["RRQ", ""])
+          while True:
+              chunk = clientSocket.recv(sockBuffer).decode()
+              print(chunk)
+              if not chunk:
+                  break
+        case "get":
+          sentence[0] = "RRQ"
+          clientSocket.send(sentence)
+          for file in sentence:
+            with open(file, 'wb') as f:
+              while True:
+                chunk = clientSocket.recv(sockBuffer) 
+                f.write(chunk)  
+                if not chunk:
+                    break
+            f.close()
+        case "end":
+          print("Connection close, client ended")
+        case _:
+          print("Unknown command")
 
-  with open(sys.argv[2], 'rb') as f:
-    while True:
-        chunk = f.read(sockBuffer)
-        if not chunk:
-            break
-        else:
-          clientSocket.send(chunk)
-  f.close()
-  # take input
-  print("client done")
+      f.close()
+      # take input
+    except KeyboardInterrupt:
+        print("Exiting!")
+        break
+    print("client done")
 
-  clientSocket.close()            # close TCP connection
+    clientSocket.close()            # close TCP connection
 
 main()
