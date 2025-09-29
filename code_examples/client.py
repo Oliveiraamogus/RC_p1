65dfa7de093fb538c4f09066cfbf0c4ccc560ba3
Manuel <mpi.oliveira@campus.fct.unl.pt>
2025-09-29 23:40:14 +0100

    work from final day before submission

--- code_examples/client.py
+++ code_examples/client.py
@@ -1,9 +1,9 @@
 """
 @author: Beatriz Ramos (68030)
-@author: Manuel Oliveira ()
+@author: Manuel Oliveira (68547)
 """
 from socket import *
-from Packet import *
+import os
 import sys
 import pickle
 
@@ -12,30 +12,62 @@ serverPort = int(sys.argv[2])       # socket server port number
 sockBuffer = 1024                   # socket buffer size
 blockSize = 512       # block size
 
+RRQ = 1
+DAT = 3
+ACK = 4
+ERR = 5
+
+class ClientProtocolError(Exception):
+  pass
 
 def dir(clientSocket): # DIR command
-  clientSocket.send(pickle.dumps(RRQ(""))) # sending RRQ packet
+  clientSocket.send(pickle.dumps((RRQ, ""))) # sending RRQ packet
+  expected_block_number = 1
   while True:
-      chunk = pickle.loads(clientSocket.recv(sockBuffer)) # receiving DAT packet
-      print(chunk.getData())
-
-      clientSocket.send(pickle.dumps(ACK(chunk.getBlock()))) # sending ACK packet
-      if chunk.getSize() == 0: # the command ends when the client receives an empty DAT packet
+    try:
+      chunk = pickle.loads(clientSocket.recv(sockBuffer)) # receiving DAT packetif chunk[0] == ERR:
+      if chunk[1] != expected_block_number :
+        raise ClientProtocolError
+      else :
+        expected_block_number += 1
+      if chunk[2] == 0: # the command ends when the client receives an empty DAT packet
           break
+      else:
+        print(chunk[3])
+        clientSocket.send(pickle.dumps((ACK, chunk[1]))) # sending ACK packet
+    except ClientProtocolError:
+      print("Protocol error,Connection closed")
+      clientSocket.close() # close TCP connection
 
 
 
 def get(clientSocket, filenames): # GET command
-  clientSocket.send(pickle.dumps(RRQ(filenames[1]))) # sending RRQ packet with the remote file name
-  with open(filenames[2], 'a') as f: # creating and openning the local file
-    while True:
-      chunk = pickle.loads(clientSocket.recv(sockBuffer)) # receiving DAT packet
-      f.write(chunk.getData()) # writing on the loacal file
-      clientSocket.send(pickle.dumps(ACK(chunk.getBlock()))) # sending ACK packet
-      if chunk.getSize() < blockSize: # the command ends when the client receives a DAT packet with less than 512 bytes
-        break
-  f.close()
-  ### não estamos a ler possiveis packets do tipo ERR
+  try:
+    with open(filenames[2], 'x') as f: # creating and openning the local file
+      clientSocket.send(pickle.dumps((RRQ, filenames[1]))) # sending RRQ packet with the remote file name
+      expected_block_number = 1
+      while True:
+        try:
+          chunk = pickle.loads(clientSocket.recv(sockBuffer)) # receiving DAT packet
+          if chunk[0] == ERR:
+            os.remove(filenames[2])
+            print("File not found")
+            break
+          f.write(chunk[3]) # writing on the local file
+          clientSocket.send(pickle.dumps((ACK, chunk[1]))) # sending ACK packet
+          if chunk[1] != expected_block_number :
+            raise ClientProtocolError
+          else :
+            expected_block_number += 1
+          if chunk[2] < blockSize: # the command ends when the client receives a DAT packet with less than 512 bytes
+            print("File transfer completed")
+            break
+        except ClientProtocolError:
+          print("Protocol error,Connection closed")
+          clientSocket.close() # close TCP connection
+    f.close()
+  except FileExistsError:
+    pass
 
 
 
@@ -48,9 +80,9 @@ def main():
     sys.exit()
 
   print("Connect to server")
-  print((pickle.loads((clientSocket.recv(sockBuffer))).getData())) # receiving server's welcoming DAT packet
-  clientSocket.send(pickle.dumps(ACK(1))) # sending ACK packet
-  sentence = [""] 
+  print((pickle.loads(clientSocket.recv(sockBuffer)))[3]) # receiving server's welcoming DAT packet
+  clientSocket.send(pickle.dumps((ACK, 1))) # sending ACK packet
+  sentence = [""]
   #### acaba de commentar esta parte pls
   while (sentence[0] != "end") :
     try:
@@ -68,8 +100,9 @@ def main():
 
       # take input
     except KeyboardInterrupt:
-        print("Exiting!")
-        break
+      print("Exiting!")
+      clientSocket.close()            # close TCP connection
+      break
 
 
 main()
--- code_examples/server.py
+++ code_examples/server.py
@@ -1,6 +1,6 @@
 """
 @author: Beatriz Ramos (68030)
-@author: Manuel Oliveira ()
+@author: Manuel Oliveira (68547)
 """
 
 import os
@@ -14,13 +14,20 @@ socketBuffer = 1024   # socket buffer size
 blockSize = 512       # block size
 dir_path = "."        # current path 
 
+RRQ = 1
+DAT = 3
+ACK = 4
+ERR = 5
+
+
+
 class ClientProtocolError(Exception):
   pass
 
 def workOnClient(socket, addr):
   # send welcoming packet with server identification 
   welMsg = "Welcome to " + str(addr) + " file server"
-  welcomePacket = (3, 1, len(welMsg), welMsg) #DAT(1, len(welMsg), welMsg) 
+  welcomePacket = (DAT, 1, len(welMsg), welMsg) #DAT(1, len(welMsg), welMsg) 
   socket.send(pickle.dumps(welcomePacket))
   # receiving client's ACK packet
   pickle.loads(socket.recv(socketBuffer))
@@ -29,27 +36,26 @@ def workOnClient(socket, addr):
       # receiving client's RRQ packets for command GET or DIR
       packet = pickle.loads(socket.recv(socketBuffer))
       # checking if it´s a RRQ packet
-      #if not isinstance(packet, RRQ):
-      if packet[0] != 1:
+      if packet[0] != RRQ:
         raise ClientProtocolError
       
-      if packet.getFilename() != "": # GET command
+      if packet[1] != "": # GET command
             try:
               # openning the file
-              with open(packet.getFilename(), 'r') as f:
-                fsize = os.path.getsize(packet.getFilename()) # get file size
+              with open(packet[1], 'r') as f:
+                fsize = os.path.getsize(packet[1]) # get file size
                 blockN = (fsize // blockSize) + 1 # calculating how many blocks we'll need for a file with fsize
                 for i in range(int(blockN)):
                   data = f.read(blockSize)
-                  datP = (3, i + 1, len(data), data)#DAT(i + 1, len(data), data)
+                  datP = (DAT, i + 1, len(data), data)#DAT(i + 1, len(data), data)
                   socket.send(pickle.dumps(datP)) # sending the DAT packet
                   ackP = pickle.loads(socket.recv(socketBuffer)) # receiving the ACK packet
                   # checking for a macth in block numbers and protocol errors
-                  if ackP[0] != 4 or i + 1 != ackP[1]:
+                  if ackP[0] != ACK or i + 1 != ackP[1]:
                     raise ClientProtocolError
                       
             except FileNotFoundError:
-              errP = (5,"File not found")#ERR("File not found")
+              errP = (ERR,"File not found")#ERR("File not found")
               socket.send(pickle.dumps(errP)) # sending ERR packet
             except ClientProtocolError:
               print("Protocol error,Connection closed")
@@ -61,27 +67,27 @@ def workOnClient(socket, addr):
         try:
           i = 1
           for path in os.listdir(dir_path):
-              if os.path.isfile(os.path.join(dir_path, path)): # check if current path is a file
-                  p = (3, 1, len(path),path)#DAT(i, len(path),path)
+              if os.path.isfile(os.path.join(dir_path, path)) and not path.startswith("."): # checks if current path is a file and if it'
+                  p = (DAT, i, len(path),path)#DAT(i, len(path),path)
                   socket.send(pickle.dumps(p)) # sending the DAT packet
                   ackP = pickle.loads(socket.recv(socketBuffer)) # receiving the ACK packet
                   # checking for a macth in block numbers and protocol errors
-                  if ackP[0] == 3 and i == ackP[1]:
+                  if ackP[0] == ACK and i == ackP[1]:
                     i = ackP[1] + 1
                   else :
                     raise ClientProtocolError
             
-          p = (3,i, 0, "")#DAT(i, 0, "")
+          p = (DAT, i, 0, "")#DAT(i, 0, "")
           socket.send(pickle.dumps(p)) #send empty DAT packet to signal that all information has been transmitted
         except ClientProtocolError:
           print("Protocol error,Connection closed")
           socket.close()# close TCP connection
         
-    except EOFError: #####Para quê esta drena?????
+    except EOFError: 
       break
     except ClientProtocolError:
             print("Protocol error,Connection closed")
-            socket.close() # close TCP connection
+            socket.close() # close TCP connection       
   socket.close() # close TCP connection
 
 
@@ -100,7 +106,8 @@ def main():
   
   while True:
     newSocket, addr = serverSocket.accept() # waits for incoming requests and new socket is created on return
-    tid = threading.Thread(target= workOnClient, args= (newSocket, addr)) # creating a thread to handle client
+    serverip = gethostbyname(gethostname())
+    tid = threading.Thread(target= workOnClient, args= (newSocket,serverip)) # creating a thread to handle client
     tid.start()
 
 main()
